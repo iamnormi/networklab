1c6f724ba7f7a068e82c5e7906fc5bb0e68e87ca
I'MVk <iamvk1437k>
2021-11-18 21:24:28 +0530

    inital code Updated

---
+++ exp2/SocketHTTPClient.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+
+public class SocketHTTPClient {
+    public static void main(String[] args) {
+        String hostName = "www.google.com";
+        int portNumber = 80;
+        try {
+            Socket socket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out.println("GET / HTTP/1.1");
+            out.println("Host: www.google.com:80");
+            out.println("Connection: Close");
+            out.println();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
+            System.exit(1);
+        }
+    }
+}
